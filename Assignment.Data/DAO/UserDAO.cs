1a538a46b2ad7ede60c6785bb43142afce63b427
Tom Lamprecht <tom.lamprecht@student.fhws.de>
2023-05-25 09:52:37 +0200

    Added UserDAO

---
+++ Assignment.Data/DAO/UserDAO.cs
@@ -0,0 +1,18 @@
+﻿using Assignment.Data.IDAO;
+using Assignment.Data.Models.Domains;
+using Assignment.Data.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Data.DAO
+{
+    public class UserDAO : IUserDAO
+    {
+        public User GetUser(AssignmentContext context, string id) {
+            return context.Users.Find(id) ?? throw new Exception("No user Found for id " + id);
+        }
+    }
+}
---
+++ Assignment.Data/IDAO/IUserDAO.cs
@@ -0,0 +1,16 @@
+﻿using Assignment.Data.Models.Domains;
+using Assignment.Data.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Data.IDAO
+{
+    public interface IUserDAO
+    {
+        User GetUser(AssignmentContext context, String id);
+
+    }
+}
