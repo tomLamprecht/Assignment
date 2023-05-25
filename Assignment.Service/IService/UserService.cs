40b4a5c71cfd77f2c1bc7545036a1c1009255a74
Tom Lamprecht <tom.lamprecht@student.fhws.de>
2023-05-25 10:15:54 +0200

    Added UserController with Dummy User

---
+++ Assignment.Service/IService/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Assignment.Data.DAO;
+using Assignment.Data.IDAO;
+using Assignment.Data.Models.Domains;
+using Assignment.Data.Models.Repository;
+using Assignment.Service.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Service.IService
+{
+    public class UserService : IUserService
+    {
+        IUserDAO userDAO;
+
+        public UserService()
+        {
+            userDAO = new UserDAO();
+        }
+
+        public User GetUser(string UserId)
+        {
+            using (AssignmentContext context = new AssignmentContext()) {
+                return userDAO.GetUser(context, UserId);
+            }
+        }
+    }
+}
---
+++ Assignment.Service/Service/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Assignment.Data.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Service.Service
+{
+    public interface IUserService
+    {
+        User GetUser(string UserId);
+
+    }
+}
