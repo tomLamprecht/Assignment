3be9135ad623aeec0272d44fdd3bb292bb1d3fff
Flixxxi <felhei@gmx.de>
2023-05-25 16:14:26 +0200

    Authentification added

--- Assignment/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Assignment/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -120,6 +120,8 @@ namespace Assignment.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, "User".ToUpper()) ;
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -10,9 +10,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+
 
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
